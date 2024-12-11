e343232d6d31ba1e4797bfda756ac2ff87b5f43d
Sebastian Kaca <sebastian.kaca@poczta.onet.pl>
2024-12-11 19:51:22 +0100

    spoj problems

---
+++ spoj/spoj/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace spoj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sum of the user number");
+
+            int number = 0;
+
+            for (int i = 1; i > 0; i++) 
+            {
+                Console.Write("Enter your number: ");
+                int userNumber = int.Parse(Console.ReadLine());
+                int result = number + userNumber;
+                Console.WriteLine(result);
+                number = result;
+            }
+        }
+    }
+}
