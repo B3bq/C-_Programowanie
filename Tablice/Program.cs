9f8bbabd0f24a56877f70057249a586edcc8751c
Sebastian Kaca <sebastian.kaca@poczta.onet.pl>
2024-12-13 19:17:32 +0100

    zadania z zajec

---
+++ Tablice/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Tablice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //pierwszy sposob deklaracji tablic
+            string[] firstNames = new string[5];
+
+            firstNames[0] = "Janek";
+            firstNames[1] = "Tom";
+            firstNames[2] = "Bob";
+            firstNames[3] = "Rob";
+            firstNames[4] = "Sue";
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(firstNames[i] + ", ");
+            }
+            Console.WriteLine();
+
+
+            firstNames[0] = "Bil";
+            Console.WriteLine(firstNames[0]);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            //drugi sposob deklaracji tablicy
+            string data = "Clark, Bruce, Arthur, Barry, Diana, Hal, Jack";
+
+            string[] firrstNamesOfDCHeroes = data.Split(',');
+
+            //ostatni element tablicy Console.WriteLine(firrstNamesOfDCHeros[firrstNamesOfDCHeros.Length-1]);
+
+            foreach (var item in firrstNamesOfDCHeroes) 
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+
+            //inicjalizacja tablicy
+
+            string[] lastNamesOfDCHeroes = new string[] {"Kent", "Wayne", "Curry", "Allen", "Prince", "Jordan"};
+
+            string[] alterEgos = new string[] {"Superman", "Batman", "Aquaman", "Flash", "Wonder Woman", "Hulk"};
+
+            for (int i = 0; i < lastNamesOfDCHeroes.Length; i++)
+            {
+                Console.WriteLine($"{firrstNamesOfDCHeroes[i]} {lastNamesOfDCHeroes[i]} - {alterEgos[i]}");
+            }
+        }
+    }
+}
---
+++ lottoFullSymulation/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace lottoFullSymulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] randomNumbers = new int[6];
+
+            
+
+            for (int i = 0; i < randomNumbers.Length; i++)
+            {
+                int los = rnd.Next(1, 50);
+
+                while (randomNumbers.Contains(los)) 
+                {
+                    los = rnd.Next(1, 50);
+                }
+
+                randomNumbers[i] = los;
+
+            }
+
+
+            foreach (var item in randomNumbers) 
+            {
+                Console.Write($"{item}, ");
+            }
+        }
+    }
+}
