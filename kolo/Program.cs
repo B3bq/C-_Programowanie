9467ca208d0aa36c45a2c20ae6b7975a9d80ebe3
Sebastian Kaca <sebastian.kaca@poczta.onet.pl>
2024-12-18 19:44:26 +0100

    zadanie z podatkiem

---
+++ kolo/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace podatek
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Obliczenie podatku");
+            Console.Write("Podaj dochód: ");
+            string userInput = Console.ReadLine();
+
+            CalculateTax(userInput);
+        }
+
+        static void CalculateTax(string userInput)
+        {
+            int userIn = int.Parse(userInput);
+
+            if (userIn > 10000 && userIn <= 30000)
+            {
+                double result = (userIn - 10000) * 0.1;
+                Console.Write($"Podatek do zapłacenia: {result} zł");
+            }
+            else if (userIn > 30000)
+            {
+                double result = (20000 * 0.1) + (userIn - 30000) * 0.2;
+
+                Console.Write($"Podatek do zapłacenia: {result} zł");
+            }
+        }
+    }
+}
