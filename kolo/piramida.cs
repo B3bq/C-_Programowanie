9b459db8d16235a493208ea0f9ab4a06de8ca076
Sebastian Kaca <sebastian.kaca@poczta.onet.pl>
2024-12-19 22:37:47 +0100

    piramida (nie dziala do konca)

---
+++ kolo/piramida.cs
@@ -0,0 +1,35 @@
+﻿namespace przykladoweKolo
+{
+    internal class piramida
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Enter height of the piramid: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                //loop for a empty spaces
+                for (int j = 1; j <= n - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                //loop showing a triangles
+                for (int j = 1; j <= (2 * i - 1); j++)
+                {
+                    Console.Write('\u25B2');
+
+                    
+                    //a space after triangle
+                    if (j < (2 * i - 1))
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
