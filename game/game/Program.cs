8711cf5dfa7620ed524048d3b88b574991378941
B3bq <162355844+B3bq@users.noreply.github.com>
2025-01-10 19:19:18 +0100

    labirynt game

---
+++ game/game/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter key: ");
+            char key =Console.ReadKey().KeyChar;
+            
+            Console.WriteLine();
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            Console.Write($"Pressed key: {keyInfo.Key}");
+        }
+    }
+}
---
+++ game/labirynt/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace labirynt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] board = InitializationBoard();
+
+            //pozycja startowa gracza
+            int playerPositionX = 0;
+            int playerPositionY = 1;
+            int exitX = 9;
+            int exitY = 8;
+
+            ConsoleKeyInfo keyInfo;
+
+            do
+            {
+                Console.Clear();
+
+                board[playerPositionX, playerPositionY] = '@';
+                DrawBoard(board);
+
+                if (playerPositionX == exitX && playerPositionY == exitY)
+                {
+                    Console.Clear();
+                    Console.BackgroundColor = ConsoleColor.Magenta;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Winner winner chicken dinner");
+                    Console.ResetColor();
+
+                    break;
+                }
+
+                keyInfo = Console.ReadKey();
+                PlayerMove(board, ref playerPositionX, ref playerPositionY, keyInfo);
+
+
+            } while (keyInfo.Key != ConsoleKey.Escape);
+
+            
+        }
+
+        //chodzenie
+        private static void PlayerMove(char[,] board, ref int playerPositionX, ref int playerPositionY, ConsoleKeyInfo keyInfo)
+        {
+            if ((keyInfo.Key == ConsoleKey.S || keyInfo.Key == ConsoleKey.DownArrow) && (playerPositionX < board.GetLength(0) - 1) && board[playerPositionX + 1, playerPositionY] == ' ') //warunki chodzenia
+            {
+                board[playerPositionX, playerPositionY] = ' ';
+                playerPositionX++;
+            }
+            else if ((keyInfo.Key == ConsoleKey.W || keyInfo.Key == ConsoleKey.UpArrow) && (playerPositionX > 0) && board[playerPositionX - 1, playerPositionY] == ' ')
+            {
+                board[playerPositionX, playerPositionY] = ' ';
+                playerPositionX--;
+            }
+            else if ((keyInfo.Key == ConsoleKey.A || keyInfo.Key == ConsoleKey.LeftArrow) && (playerPositionY > 0) && board[playerPositionX, playerPositionY - 1] == ' ')
+            {
+                board[playerPositionX, playerPositionY] = ' ';
+                playerPositionY--;
+            }
+            else if ((keyInfo.Key == ConsoleKey.D || keyInfo.Key == ConsoleKey.RightArrow) && (playerPositionY < board.GetLength(1) - 1) && board[playerPositionX, playerPositionY + 1] == ' ')
+            {
+                board[playerPositionX, playerPositionY] = ' ';
+                playerPositionY++;
+            }
+        }
+
+        //wyswietlanie mapy
+        private static void DrawBoard(char[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    Console.Write(board[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        //incjalizacja mapy
+        private static char[,] InitializationBoard()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            return new char[,]{
+                { '#', ' ', '#', '#', '#', '#', '#', '#', '#', '#' },
+                { '#', ' ', '#', ' ', ' ', ' ', '#', ' ', ' ', '#' },
+                { '#', ' ', '#', '#', '#', ' ', '#', '#', ' ', '#' },
+                { '#', ' ', '#', ' ', '#', ' ', ' ', '#', ' ', '#' },
+                { '#', ' ', '#', ' ', '#', '#', ' ', '#', ' ', '#' },
+                { '#', ' ', '#', ' ', '#', '#', ' ', '#', ' ', '#' },
+                { '#', ' ', '#', ' ', ' ', ' ', ' ', '#', ' ', '#' },
+                { '#', ' ', '#', '#', '#', ' ', '#', '#', ' ', '#' },
+                { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+                { '#', '#', '#', '#', '#', '#', '#', '#', ' ', '#' },
+            };
+        }
+    }
+}
