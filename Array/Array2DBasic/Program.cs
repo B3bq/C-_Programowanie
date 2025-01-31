af69b69f23c42126aa740f1c72acf80de209a455
B3bq <162355844+B3bq@users.noreply.github.com>
2025-01-31 12:10:59 +0100

    Arrays

---
+++ Array/Array2DBasic/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Array2DBasic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //tworzenie tablicy 2 wymiarowej
+            int[,] array2D = new int[2, 3];
+
+            array2D[0, 0] = 1;
+            array2D[0, 1] = 2;
+            array2D[0, 2] = 3;
+            array2D[1, 0] = 4;
+            array2D[1, 1] = 5;
+            array2D[1, 2] = 6;
+
+            Console.WriteLine(array2D[0, 0] + " " + array2D[0, 1]);
+            Console.WriteLine($"Array lenght: {array2D.Length}");
+            Console.WriteLine($"Array row lenght: {array2D.GetLength(0)}");
+            Console.WriteLine($"Array column lenght: {array2D.GetLength(1)}");
+
+            for (int i = 0; i < array2D.GetLength(0); i++)
+            {
+                for (int j = 0; j < array2D.GetLength(1); j++)
+                {
+                    Console.Write(array2D[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            //deklarowanie wartosci startowych tabklicy 2 wymiarowej
+
+            string[,] textArray2D =
+                {
+                    {"Hello", "World"},
+                    {"Nice", "Day"}
+                };
+
+            //Nice World
+            Console.Write($"{textArray2D[1,0]} {textArray2D[0,1]}");
+            Console.WriteLine();
+
+            //za ppomoca jednej petli mozna wypisac elementy tablicy
+            foreach (var item in textArray2D)
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
---
+++ Array/Array2DStatistic/Program.cs
@@ -0,0 +1,108 @@
+﻿namespace Array2DStatistic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter array dimiention: ");
+            int n = int.Parse(Console.ReadLine());
+
+            //deklaracja macierzy nxn
+            int[,] matrix = new int[n, n];
+
+            Random rnd = new Random();
+
+            //uzupelnienie macierzy
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = rnd.Next(n * n);
+                }
+            }
+
+
+            //wydruk macierzy
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{matrix[i,j], 3} ");//sformatowane, zeby kolumny landnie wygladaly
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            //min wartosc
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("-----Min Values----------");
+            
+            
+            for (int j = 0; j < n; j++)
+            {
+                int minValue = matrix[0, j];
+
+                for (int i = 1; i < n; i++)
+                {
+                    if (matrix[i, j] < minValue)
+                    {
+                        minValue = matrix[i, j];
+                    }
+                }
+                Console.Write($"{minValue,3}");
+            }
+            
+            
+            Console.ResetColor();
+
+            Console.WriteLine();
+
+            //max wartosc
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("-----Max Values----------");
+
+
+            for (int j = 0; j < n; j++)
+            {
+                int maxValue = matrix[0, j];
+
+                for (int i = 1; i < n; i++)
+                {
+                    if (matrix[i, j] > maxValue)
+                    {
+                        maxValue = matrix[i, j];
+                    }
+                }
+                Console.Write($"{maxValue,3}");
+            }
+
+
+            Console.ResetColor();
+            Console.WriteLine();
+
+            //srednia arytmetyczna macierzy
+            int sum = 0;
+
+            foreach (int item in matrix) 
+            { 
+                 sum += item;
+            }
+            double mean = (double)sum / matrix.Length;
+            Console.Write($"Avrage of matrix: {mean:F2}");//F liczba miejsc po przecinku
+
+            Console.WriteLine();
+
+            //ile elementow jest mniejszych niz srednia
+
+            int counter = 0;
+
+            foreach (var item in matrix)
+            {
+                if (item < mean) { counter++; }
+            }
+            Console.Write($"How many numbers is lower than avg: {counter}");
+        }
+    }
+}
