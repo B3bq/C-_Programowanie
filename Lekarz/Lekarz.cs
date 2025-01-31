42b77c478af0ec9950d777ee82c2ed9d07862748
B3bq <162355844+B3bq@users.noreply.github.com>
2025-01-31 18:59:31 +0100

    kolos2 zajecia

---
+++ Lekarz/Lekarz.cs
@@ -0,0 +1,131 @@
+﻿namespace Lekarz
+{
+    internal class Lekarz
+    {
+        static void Main(string[] args)
+        {
+            List<string> pacjent = new List<string>();
+            bool isExitPressed = false;
+
+            while (isExitPressed == false)
+            {
+                Console.Clear();
+                string userChoice = DoctorQueueMenu();
+                
+                switch (userChoice.ToLower())
+                {
+                    case "d":
+                        AddPatient(pacjent);
+                        Console.Write("Kliknij dowolny przycisk, aby kontynułować...");
+                        Console.ReadKey();
+                        break;
+                    case "p":
+                        ServePatient(pacjent);
+                        Console.Write("Kliknij dowolny przycisk, aby kontynułować...");
+                        Console.ReadKey();
+                        break;
+                    case "l":
+                        DisplayQueue(pacjent);
+                        Console.Write("Kliknij dowolny przycisk, aby kontynułować...");
+                        Console.ReadKey();
+                        break;
+                    case "s":
+                        GetPatientsBefore(pacjent);
+                        Console.Write("Kliknij dowolny przycisk, aby kontynułować...");
+                        Console.ReadKey();
+                        break;
+                    case "w":
+                        isExitPressed = true;
+                        break;
+                    default:
+                        Console.WriteLine("Brak tekiej opcji w menu");
+                        Console.ReadKey();
+                        Console.Write("Kliknij dowolny przycisk, aby kontynułować...");
+                        break;
+                } 
+            }
+        }
+
+        private static void GetPatientsBefore(List<string> pacjent)
+        {
+            Console.Write("Imię i nazwisko pacjenta: ");
+            string namePatient = Console.ReadLine();
+            int counter = 0;
+            if (pacjent.Contains(namePatient))
+            {
+                for (int i = 0; i < pacjent.Count(); i++)
+                {
+                    if (pacjent[i] != namePatient)
+                    {
+                        counter++;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Liczba osób w kolejce przed: {counter}");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Brak pacjenta o takim imieniu i nazwisku");
+            }
+        }
+
+        private static void ServePatient(List<string> pacjent)
+        {
+            if (pacjent.Count == 0)
+            {
+                Console.WriteLine("Brak pacjentów do przyjęcia. Dzień wolny :)");
+            }
+            else
+            {
+                Console.WriteLine($"{pacjent[0]}");
+                pacjent.RemoveAt(0);
+            }
+        }
+
+        private static void AddPatient(List<string> pacjent)
+        {
+            Console.Write("Imię i nazwisko pacjenta: ");
+            string pacient = Console.ReadLine();
+            if (pacient == string.Empty)
+            {
+                Console.WriteLine("Podaj imię i nazwisko pacjenta");
+            }
+            else
+            {
+                pacjent.Add(pacient);
+                Console.WriteLine("Dodano pacjenta do listy");
+            }
+        }
+
+        private static void DisplayQueue(List<string> pacjent)
+        {
+            if (pacjent.Count() == 0) 
+            {
+                Console.WriteLine("Brak pacjentów na liście");
+            }
+            else
+            {
+                for (int i = 0; i < pacjent.Count() ; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {pacjent[i]}");
+                }
+            }
+        }
+
+        private static string DoctorQueueMenu()
+        {
+            Console.WriteLine("\nDoctor queue app");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("[D]odaj pacjent do kolejki");
+            Console.WriteLine("[P]rzyimj pacjenta");
+            Console.WriteLine("[L]ista pacjentów oczekujących");
+            Console.WriteLine("[S]prawdź który w kolejce");
+            Console.WriteLine("[W]yjdź");
+            Console.Write("> ");
+            string userChoice = Console.ReadLine();
+            return userChoice;
+        }
+    }
+}
---
+++ Opady/Opady.cs
@@ -0,0 +1,85 @@
+﻿namespace Opady
+{
+    internal class Opady
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] iloscOpadow = new int[7];
+            for (int i = 0; i < iloscOpadow.Length; i++)
+            {
+                int los = rnd.Next(0, 31);
+                iloscOpadow[i] = los;
+            }
+
+            string[] dniTygodnia = { "Pon", "Wto", "Śro", "Czw", "Pią", "Sob", "Nie" };
+
+            Console.WriteLine("Analiza opadów deszczu");
+
+            int liczbadni = Question();
+            GetRainfallStatistics(iloscOpadow, dniTygodnia, liczbadni);
+
+        }
+
+        private static void GetRainfallStatistics(int[] iloscOpadow, string[] dniTygodnia, int liczbadni)
+        {
+            for (int i = 0; i < liczbadni; i++)
+            {
+                Console.Write("\n" + dniTygodnia[i] + ": ");
+                for (int j = 0; j < iloscOpadow[i]; j++)
+                {
+                    Console.Write('\u2588');
+                }
+                Console.Write($" {iloscOpadow[i]} mm");
+            }
+
+            int sumRainfall = 0;
+            for (int i = 0; i < liczbadni; i++) 
+            {
+                sumRainfall += iloscOpadow[i];
+            }
+
+            double avgRainfall = sumRainfall / liczbadni;
+
+            int maxValue = iloscOpadow[0];
+            string maxDay = dniTygodnia[0];
+            for (int i = 0; i < liczbadni; i++) 
+            {
+                if (iloscOpadow[i] > maxValue) 
+                {
+                    maxValue = iloscOpadow[i];
+                    maxDay = dniTygodnia[i];
+                }
+            }
+
+            int minValue = iloscOpadow[0];
+            string minDay = dniTygodnia[0];
+            for (int i = 0; i < liczbadni; i++)
+            {
+                if (iloscOpadow[i] < minValue)
+                {
+                    minValue = iloscOpadow[i];
+                    minDay = dniTygodnia[i];
+                }
+            }
+
+            Console.WriteLine($"\nNajwieksza ilość opadów: {maxDay} ({maxValue} mm)");
+            Console.WriteLine($"Najminejsza ilość opadów: {minDay} ({minValue} mm)");
+            Console.WriteLine($"Średnia ilość opadów: {avgRainfall:F2}");
+        }
+
+        private static int Question()
+        {
+            while (true)
+            {
+                Console.Write("Poadj liczbę dni tygodnia: ");
+                string liczbadni = Console.ReadLine();
+                if (int.TryParse(liczbadni, out int number) && (number > 0 && number <=7))
+                {
+                    return number;
+                }
+                Console.WriteLine("Wprowadź liczb z zakresu 1-7"); 
+            }
+        }
+    }
+}
